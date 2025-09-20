8a2d919c8538901cb2908b8a3ee5bf0749994933
mahii47 <mahmankar333@gmail.com>
2025-09-20 16:41:17 +0530

    Intial commit- Selenium Login Test

---
+++ src/test/java/Tests/LoginTest.java
@@ -0,0 +1,37 @@
+package Tests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+    }
+
+    @Test
+    public void testLogin() {
+        driver.findElement(By.id("username")).sendKeys("student");
+        driver.findElement(By.id("password")).sendKeys("Password123");
+        driver.findElement(By.id("submit")).click();
+
+        WebElement message = driver.findElement(By.tagName("h1"));
+        Assert.assertEquals(message.getText(), "Logged In Successfully");
+    }
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
